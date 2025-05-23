0809c4e5f72feffe27fbc83542cbe7b7075a928d
ravi-nder1987 <ravindersuryavanshi85@gmail.com>
2025-05-24 00:02:23 +0530

    Add files via upload

---
+++ Assignment2_1.py
@@ -0,0 +1,7 @@
+#Program to validate number is even or odd
+
+num=int(input('Enter a number:'))
+if num%2 == 1:
+    print(str(num) + " is an odd number")
+else:
+    print(str(num) + " is an even number")
---
+++ Assignment2_2.py
@@ -0,0 +1,5 @@
+#Program to print sum of integers from 1 to 50
+sum = 0
+for i in range(1,51):
+    sum = sum + i
+print("The sum of number from 1 to 50 is: " + str(sum))
